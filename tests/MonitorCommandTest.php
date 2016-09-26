d55402e20c475a9fa771fb7f4f9546ede1a0afb0
Iacovos Constantinou <softius@gmail.com>
2016-09-26 15:13:50 +0300

    Add monitor command

---
+++ tests/MonitorCommandTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Softius\JenkinsJobMonitor;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class MonitorCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBasicUsage()
+    {
+        // Create a mock for Guzzle Client
+        $mock = new MockHandler([new Response(200)]);
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+
+        // Create the command and
+        $command = new MonitorCommand();
+        $command->setClient($client);
+        $command->setApplication(new Application());
+        $commandTester = new CommandTester($command);
+        $exitCode = $commandTester->execute(['command' => 'monitor', 'url' => 'http://test.com', 'job' => 'test', 'monitor' => 'echo 1']);
+        $this->assertEquals(0, $exitCode);
+    }
+}
